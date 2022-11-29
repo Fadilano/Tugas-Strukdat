0b0e06ec6a4fda5653e9b942cac8932104f2672b
rayhanpasya <116943139+rayhanpasya@users.noreply.github.com>
2022-11-29 11:12:43 +0700

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,13 +1,17 @@
 fnama = open("nama.txt", "r")
 fnpm = open("npm.txt", "r")
+fkelas = open("kelas.txt", "r")
+fjurusan = open("jurusan.txt", "r")
 
 readnama = fnama.readlines()
 readnpm = fnpm.readlines()
+readkelas = fkelas.readlines()
+readjurusan = fjurusan.readlines()
 
 print("""
-+--------------------+-------------------+
-| NAMA               | NPM               |
-+--------------------+-------------------+""")
++--------------------+-------------------------------+----------------------+
+| NAMA               | NPM               |KELAS              |JURUSAN        |
++--------------------+-------------------------------------------------+-------------+""")
 
 for i in range (len(readnama)):
     nama = str(readnama[i].strip())
@@ -18,7 +22,15 @@ for i in range (len(readnama)):
     print('| '+npm,end='')
     for k in range(19-1-len(npm)):
          print(' ',end ='')
+    kelas = str(readkelas[i].strip())
+    print('| '+kelas,end='')
+    for p in range(19-1-len(kelas)):
+         print(' ',end ='')
+    jurusan = str(readjurusan[i].strip())
+    print('| '+jurusan,end='')
+    for v in range(19-1-len(jurusan)):
+         print(' ',end ='')
     
     print('|')
 
-print("+--------------------+-------------------+")
+print("+--------------------+-------------------------------------------------------+")
