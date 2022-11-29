2a3bed486b4ea1a239dc4e8173048ffc26cd2492
rayhanpasya <116943139+rayhanpasya@users.noreply.github.com>
2022-11-29 11:11:43 +0700

    Update createFile.py

--- createFile.py
+++ createFile.py
@@ -1,5 +1,7 @@
 nama = []
 npm = []
+kelas = []
+jurusan = []
 print("===============TUGAS STRUKDAT===============")
 jml_data = int(input("Masukan Jumlah data : "))
 
@@ -8,9 +10,13 @@ for i in range(jml_data):
     print("===========Data Mahasiswa ke -",i,"===========")
     nm = str(input("Masukan Nama    : "))
     np = (input("Masukan NPM     : "))
+    kls = (input("Masukan Kelas    : "))
+    jrsn = (input("Masukan Jurusan    : "))
     print("===========================================")
     nama.append(nm)
     npm.append(np)
+    kelas.append(kls)
+    jurusan.append(jrsn)
     
 with open('nama.txt', 'w') as fnama:
     for line in nama:
@@ -20,3 +26,11 @@ with open('npm.txt', 'w') as fnpm:
     for line in npm:
         fnpm.write(f"{line}\n")
 fnpm.close()
+with open('kelas.txt', 'w') as fkelas:
+    for line in kelas:
+        fkelas.write(f"{line}\n")
+fkelas.close()
+with open('jurusan.txt', 'w') as fjurusan:
+    for line in jurusan:
+        fjurusan.write(f"{line}\n")
+fjurusan.close()
