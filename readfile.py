f5dfa1e5f04dcae48593434b9f3b59c2e5bd682c
rayhanpasya <116943139+rayhanpasya@users.noreply.github.com>
2022-12-02 20:51:26 +0700

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -7,15 +7,18 @@ readnama = fnama.readlines()
 readnpm = fnpm.readlines()
 readkelas = fkelas.readlines()
 readjurusan = fjurusan.readlines()
-
+#Line 6-9 ini fungsinya sebagai membaca file txt nya berupa string
 print("""
 +--------------------+-----------------+-----------+---------------+
 | NAMA               | NPM             |KELAS      |JURUSAN        |
 +--------------------+-----------------+-----------+---------------+""")
-
+# Kemudian Membuat tabel dengan Print di design seperti code diatas.
 for i in range (len(readnama)):
     nama = str(readnama[i].strip())
     print('| '+nama,end='')
+    #perulangan for dengan variabel i in range atau dalam jarak seberapa panjang, variabel nama tersebut
+    #dan variabel nama = string readnama yang dimasukkan variabel i perulangannya dan strip sebagai menghasilkan karakternya besar atau kecil
+    #kemudian cetak +nama,end='' artinya mencetak dengan membuat baris baru ketika mencetak lagi.
     for j in range(20-1-len(nama)):
         print(' ',end ='')
     npm = str(readnpm[i].strip())
@@ -30,7 +33,12 @@ for i in range (len(readnama)):
     print('| '+jurusan,end='')
     for v in range(15-1-len(jurusan)):
          print(' ',end ='')
+    #selanjutnya masih sama seperti tadi perulangan for dengan variabel j, k, p, v dalam jarak 20-1-len dan 19-1-len dimasukkan variabel nya masing masing disesuaikan
+    #mencetak dengan print untuk baris barunya dan npm = string readnpm yang dimasukkan variabel i perulangannya dan strip sebagai menghasilkan karakternya besar atau kecil
+    #lalu cetak lagi dengan akhiran end=
     
     print('|')
+    #sebagai penutup dari tabel outputan
 
 print("+--------------------+-----------------+-----------+---------------+")
+#sebagai bagian bawah dari tabel
