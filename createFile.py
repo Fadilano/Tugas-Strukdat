28fe096514736f5d6e23f0bd553b138f0f2a5f44
MaulanaBintang <117064989+MaulanaBintang@users.noreply.github.com>
2022-12-02 20:53:19 +0700

    Update createFile.py

--- createFile.py
+++ createFile.py
@@ -1,10 +1,21 @@
+"""
+Pada line 5-9 disini adalah pendeklarasian variable yang digunakan dan judul dari program ini, Setelah itu pada line ke 10 buat perintah 
+untuk user memasukkan jumlah data yang diiinginkan dan nilai ini akan disimpan sebagai nilai dari variable jml_data
+"""
 nama = []
-npm = []
-kelas = []
-jurusan = []
+npm = []                
+kelas = [] 
+jurusan = []                                                     
 print("===============TUGAS STRUKDAT===============")
 jml_data = int(input("Masukan Jumlah data : "))
-
+"""
+Selanjutnya masuk kepada perulangan pada program ini, perulangannya ditentukan dari berapa jumlah data yang diinginkan user tadi yang disimpan 
+pada variable jml_data tadi. Maka untuk kondisi fornya bisa menggunakan kondisi 
+for i in range(jml_data):
+    i+=1 
+Dan perulangannya berisi tentang perintah ntuk uuser menginputkan data data yang dimasukkan nantinya yaitu sesuuai variabel diatas tadi yaitu
+Nama, Kelas, NPM, Dan Jurusan. 
+"""
 for i in range(jml_data):
     i+=1
     print("===========Data Mahasiswa ke -",i,"===========")
@@ -24,7 +35,7 @@ with open('nama.txt', 'w') as fnama:
 fnama.close()
 with open('npm.txt', 'w') as fnpm:
     for line in npm:
-        fnpm.write(f"{line}\n")
+        fnpm.write(f"{line}\n")                                  
 fnpm.close()
 with open('kelas.txt', 'w') as fkelas:
     for line in kelas:
